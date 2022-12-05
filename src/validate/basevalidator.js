420430f782fae341e0420f916af05e4be1ba0cdb
dochinhlatoi113 <93257655+dochinhlatoi113@users.noreply.github.com>
2022-12-05 17:25:46 +0700

    updated

--- src/validate/basevalidator.js
+++ src/validate/basevalidator.js
@@ -1,24 +1,22 @@
 const {check, validationResult } = require('express-validator');
 
 let validateDepartment = (req,res) => {
- check('name', 'username does not Empty').not().isEmpty()
- const err = validationResult(req) 
- if(!err.isEmpty()){
-    return res.json(err.array())
- }
+    let name = req.body.name;  
+    req.checkBody('name', 'Name is required').notEmpty();
+    var errors = req.validationErrors();
+    if(errors){
+       req.session.errors = errors;
+       req.session.success = false;
+       res.send('error');
+    }
+    else{
+       req.session.success = true;
+       res.send('oke');
+    }
 }
-
-let validateLogin = () => {
-  return [ 
-    check('user.email', 'Invalid does not Empty').not().isEmpty(),
-    check('user.email', 'Invalid email').isEmail(),
-    check('user.password', 'password more than 6 degits').isLength({ min: 6 })
-  ]; 
-}
-
 let checkvalidate = {
   validateDepartment: validateDepartment,
-  validateLogin: validateLogin
+  
 };
 
 module.exports = {checkvalidate};
