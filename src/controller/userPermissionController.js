37c405b1c32fc76f07555e760f6120496f666188
dochinhlatoi113 <93257655+dochinhlatoi113@users.noreply.github.com>
2022-12-16 17:22:22 +0700

    updated

--- src/controller/userPermissionController.js
+++ src/controller/userPermissionController.js
@@ -1,213 +1,234 @@
 const db = require("../models/index")
-const { QueryTypes,Op } = require("sequelize");
+const { QueryTypes, Op } = require("sequelize");
 const checkArr = require("../helper/isset")
-let show = async (req, res) => {   
+let show = async (req, res) => {
     let keyWord = req.query.keyWord;
     let itemPerPage = 3;
     let page = +req.query.page || 1
-    
-    let offset = (page-1) * itemPerPage
-   try {
-    if(keyWord != undefined) {
-        let totalItems = await db.user_permission.count({
-            include:[{
-                model:db.departments
-            },
-            {
-                model:db.permissions
-            },
-            {
-                model:db.Admin
-            }],
-            where: {  '$Admin.email$': { [Op.like]: `%${keyWord}%`} },
-        })
-       
-        let lists =  await db.user_permission.findAll({
-           
-            include:[{
-                model:db.departments
-            },
-            {
-                model:db.permissions
-            },
-            {
-                model:db.Admin
-            }],
-            where: { '$Admin.email$': { [Op.like]: `%${keyWord}%`} },
-            limit: itemPerPage,
-            offset:offset,
-            order: [['id', 'DESC']],
-        });
-      
-        let data = {
-            lists: lists,
-            currentPage: page,
-            hasNextPage: (itemPerPage * page) < totalItems,
-            hasPreviousPage: page > 1,
-            nextPage: page + 1,
-            keyWord:keyWord,
-            previousPage: page - 1,
-            lastPage: Math.ceil(totalItems / itemPerPage),
-            message:req.flash('message')
-        }
-        res.render("../views/group/user-permission/show.handlebars", data)
-    }else{
-        let totalItems = await db.user_permission.count({
-            
-          
-        })
-        let lists =  await db.user_permission.findAll({
-            include:[{
-                model:db.departments
-            },
-            {
-                model:db.permissions
-            },
-            {
-                model:db.Admin
-            }],
-            limit: itemPerPage,
-            offset:offset,
-            order: [['id', 'DESC']],
-        });
-        let data = {  
-            lists: lists,
-            currentPage: page,
-            hasNextPage: (itemPerPage * page) < totalItems,
-            hasPreviousPage: page > 1,
-            nextPage: page + 1,
-            previousPage: page - 1,
-            lastPage: Math.ceil(totalItems / itemPerPage),
-            message:req.flash('message')
-        }
-        res.render("../views/group/user-permission/show.handlebars", data)
-    }
-  
-   } catch (error) {
-      error
-   }
-}
-let create = async(req,res) => {
-    let listsUserPermission =  await db.user_permission.findAll({
-           
-        include:[
-        {
-            model:db.Admin
-        }],
-        order: [['id', 'DESC']],
-    });
-  
-    let listsUser =  await db.Admin.findAll({
-        
-    })
-    
-    let listsPermissions =  await db.permissions.findAll({
-    
-    })
-    let listsDepartments =  await db.departments.findAll({
-    
-    })
-    data = {
-        listsUser :listsUser,
-        listsUserPermission,
-        listsPermissions:listsPermissions,
-        listsDepartments:listsDepartments,
-        message:req.flash('message',['err','ok'])
-    }
-    res.render("../views/group/user-permission/create.handlebars",{data})
-}
-let store =async(req, res) => {
-    try {   
-        let user =await db.user_permission.findOne({ where: { userId: req.body.user } })
-        if(user){
-            req.flash('message', 'eerr');
-           return res.redirect("/group/user-permission/create")
-        }else{
-            for(let i = 0; i < req.body.permissions.length ; i++ ){
-                const permission = await db.user_permission.create({ permissionId:req.body.permissions[i],departmentId:req.body.department,userId:req.body.user});
-            }
-        }
-      
-          
-        req.flash('message', 'saved successfully');
-        res.redirect("/group/user-permission/create")
-     
-      }catch (err) {
-        res.send(err);
-      }
-}
 
-let edit =async (req,res) => {
-   
-    let id = req.params.id
-    let listsUser =  await db.Admin.findAll({
-        
-    })
-    
-    let listsPermissions =  await db.permissions.findAll({
-    
-    })
-    let listsDepartments =  await db.departments.findAll({
-    
-    })
-    let listUserPermissions = await db.user_permission.findAll({
-        where:{id:id},
+    let offset = (page - 1) * itemPerPage
+    try {
+        if (keyWord != undefined) {
+            let totalItems = await db.user_permission.count({
+                include: [{
+                    model: db.departments
+                },
+                {
+                    model: db.permissions
+                },
+                {
+                    model: db.Admin
+                }],
+                where: { '$Admin.email$': { [Op.like]: `%${keyWord}%` } },
+            })
+
+            let lists = await db.user_permission.findAll({
+
+                include: [{
+                    model: db.departments
+                },
+                {
+                    model: db.permissions
+                },
+                {
+                    model: db.Admin
+                }],
+                where: { '$Admin.email$': { [Op.like]: `%${keyWord}%` } },
+                limit: itemPerPage,
+                offset: offset,
+                order: [['id', 'DESC']],
+            });
+
+            let data = {
+                lists: lists,
+                currentPage: page,
+                hasNextPage: (itemPerPage * page) < totalItems,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                keyWord: keyWord,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalItems / itemPerPage),
+                message: req.flash('message')
+            }
+            res.render("../views/group/user-permission/show.handlebars", data)
+        } else {
+            let totalItems = await db.user_permission.count({
+
+
+            })
+            let lists = await db.user_permission.findAll({
+                include: [{
+                    model: db.departments
+                },
+                {
+                    model: db.permissions
+                },
+                {
+                    model: db.Admin
+                }],
+                limit: itemPerPage,
+                offset: offset,
+                order: [['id', 'DESC']],
+            });
+            let data = {
+                lists: lists,
+                currentPage: page,
+                hasNextPage: (itemPerPage * page) < totalItems,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalItems / itemPerPage),
+                message: req.flash('message')
+            }
+            res.render("../views/group/user-permission/show.handlebars", data)
+        }
+
+    } catch (error) {
+        error
+    }
+}
+let create = async (req, res) => {
+    let listsUserPermission = await db.user_permission.findAll({
+
         include: [
             {
-                model:db.departments
+                model: db.Admin
+            }],
+        order: [['id', 'DESC']],
+    });
+
+    let listsUser = await db.Admin.findAll({
+
+    })
+
+    let listsPermissions = await db.permissions.findAll({
+
+    })
+    let listsDepartments = await db.departments.findAll({
+
+    })
+    data = {
+        listsUser: listsUser,
+        listsUserPermission,
+        listsPermissions: listsPermissions,
+        listsDepartments: listsDepartments,
+        message: req.flash('message'),
+        messageErr: req.flash('messageErr'),
+    }
+    res.render("../views/group/user-permission/create.handlebars", { data })
+}
+let store = async (req, res) => {
+    try {
+        let user = await db.user_permission.findOne({ where: { userId: req.body.user } })
+        if (user) {
+            req.flash('messageErr', 'đã phân quyền cho account này rồi');
+            return res.redirect("/group/user-permission/create")
+        } else {
+            for (let i = 0; i < req.body.permissions.length; i++) {
+                const permission = await db.user_permission.create({ permissionId: req.body.permissions[i], departmentId: req.body.department, userId: req.body.user });
+            }
+        }
+
+
+        req.flash('message', 'saved successfully');
+        res.redirect("/group/user-permission/create")
+
+    } catch (err) {
+        res.send(err);
+    }
+}
+
+let edit = async (req, res) => {
+
+    let id = req.params.id
+    let listsUser = await db.Admin.findAll({
+
+    })
+
+    let listsPermissions = await db.permissions.findAll({
+
+    })
+    let listsDepartments = await db.departments.findAll({
+
+    })
+    let listUserPermissions = await db.user_permission.findOne({
+        where: { id: id },
+        include: [
+            {
+                model: db.departments
             },
             {
-                model:db.permissions
+                model: db.permissions
             },
             {
-                model:db.Admin
+                model: db.Admin
             }
         ]
     })
-    
-    if(listUserPermissions){
+
+    if (listUserPermissions) {
         let data = {
-            listsUser :listsUser,
-            listsPermissions:listsPermissions,
-            listsDepartments:listsDepartments,
-            listUserPermissions:listUserPermissions,
-            message:req.flash('message')
+            listsUser: listsUser,
+            listsPermissions: listsPermissions,
+            listsDepartments: listsDepartments,
+            listUserPermissions: listUserPermissions,
+            message: req.flash('message'),
+            messageErr: req.flash('messageErr'),
         }
-        res.render("../views/group/user-permission/edit.handlebars",data)
-    }else{
-        res.render("../views/error/error.handlebars",{layout: null})
+        res.render("../views/group/user-permission/edit.handlebars", data)
+    } else {
+        res.render("../views/error/error.handlebars", { layout: null })
     }
 }
-let update = async(req,res) => {
+let update = async (req, res) => {
     let id = req.params.id
- 
+    // console.log(req.body.permissions)
+    // console.log(req.body.permissionId)
+    // console.log(id)
+    // return false
     try {
-       await db.departments.update(
-                { name: req.body.name },
-                { where: { id: id } }
-            )
-            req.flash('message', 'updated successfully');
-            res.redirect("/group/user-permission/edit/"+id)
-      }catch (err) {
+        let user = await db.user_permission.findOne({ where: { id: id, permissionId: req.body.permissions } }) 
+   
+        if (user) {
+            req.flash('messageErr', 'đã tồn tại quyền của account này');
+            return res.redirect("/group/user-permission/edit/"+id)
+        }else{
+            let userId = await db.user_permission.findOne({ where: { userId:req.body.userIds , permissionId:req.body.permissions } }) 
+            if(userId){
+                req.flash('messageErr', 'đã tồn tại quyền của account này');
+                 return res.redirect("/group/user-permission/edit/"+id)
+            }else{
+                for (let i = 0; i < req.body.permissions.length; i++) {
+               console.log(222)
+               console.log(id)
+              }
+              return false
+                req.flash('message', 'updated successfully');
+                return res.redirect("/group/user-permission/edit/"+id)
+            }
+        }
+              
+         
+  
+    } catch (err) {
         res.send(err);
-      }
+    }
 }
-let destroy = async(req, res) => {
+let destroy = async (req, res) => {
     let id = req.params.id
     try {
         await db.user_permission.destroy(
-        
+
             { where: { id: id } }
         )
-            req.flash('message', 'delete successfully');
-            res.redirect("/group/user-permission/")
-      }catch (err) {
+        req.flash('message', 'delete successfully');
+        res.redirect("/group/user-permission/")
+    } catch (err) {
         res.send(err);
-      }
+    }
 }
 
 
 module.exports = {
-    show,create, store, edit,update,destroy
+    show, create, store, edit, update, destroy
 }
-       
